8fb8df4669e8d3b15f5e6688be79362e01a830ff
Wonjae Lee <wonjae.lee.2@gmail.com>
2024-05-14 06:16:57 +0000

    feat: load dbt models as dagster assets

--- fusion_etl/__init__.py
+++ fusion_etl/__init__.py
@@ -1,6 +1,7 @@
-from dagster import Definitions, EnvVar
+from dagster import Definitions, EnvVar, load_assets_from_modules
 from dagster_dbt import DbtCliResource
 
+from .assets import dbt
 from .assets.mappings import RDP_MAPPINGS
 from .assets.rdp import define_blob_rdp_asset, define_src_rdp_asset
 from .resources.azure import AzureBlobResource
@@ -15,6 +16,7 @@ source_rdp_assets = [
     define_src_rdp_asset(rdp_mapping, EnvVar("DAGSTER_ENV"))
     for rdp_mapping in RDP_MAPPINGS
 ]
+dbt_assets = load_assets_from_modules([dbt])
 
 azure_blob_resource = AzureBlobResource(
     account_url=EnvVar("AZURE_STORAGE_URL"),
@@ -32,10 +34,11 @@ fusion_resource = FusionResource(
     fusion_server=EnvVar("FUSION_SERVER"),
     fusion_database=EnvVar("FUSION_DATABASE"),
 )
-dbt_resource = DbtCliResource(project_dir=EnvVar("DBT_PROJECT_DIR").get_value())
+dbt_project_dir = EnvVar("DBT_PROJECT_DIR").get_value()
+dbt_resource = DbtCliResource(project_dir=dbt_project_dir)
 
 defs = Definitions(
-    assets=blob_rdp_assets + source_rdp_assets,
+    assets=[*blob_rdp_assets, *source_rdp_assets, *dbt_assets],
     resources={
         "azure_blob_resource": azure_blob_resource,
         "rdp_resource": rdp_resource,
---
+++ fusion_etl/assets/dbt.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Any, Mapping
+
+from dagster import AssetExecutionContext, AssetKey, EnvVar
+from dagster_dbt import DagsterDbtTranslator, DbtCliResource, dbt_assets
+
+
+dbt_project_dir = Path(EnvVar("DBT_PROJECT_DIR").get_value())
+dagster_env = EnvVar("DAGSTER_ENV").get_value()
+dbt_resource = DbtCliResource(project_dir=dbt_project_dir)
+dbt_manifest_path = (
+    dbt_resource.cli(["parse", "--quiet", "-t", dagster_env])
+    .wait()
+    .target_path.joinpath("manifest.json")
+)
+
+
+class CustomDagsterDbtTranslator(DagsterDbtTranslator):
+    def get_asset_key(self, dbt_resource_props: Mapping[str, Any]) -> AssetKey:
+        resource_type = dbt_resource_props["resource_type"]
+        name = dbt_resource_props["name"]
+
+        if resource_type == "source":
+            source_name = dbt_resource_props["source_name"]
+            return AssetKey(f"src_{source_name}__{name}").with_prefix("src")
+        else:
+            return super().get_asset_key(dbt_resource_props).with_prefix("dbt")
+
+
+@dbt_assets(
+    manifest=dbt_manifest_path, dagster_dbt_translator=CustomDagsterDbtTranslator()
+)
+def dbt_models(context: AssetExecutionContext, dbt_resource: DbtCliResource):
+    yield from dbt_resource.cli(["build", "-t", dagster_env], context=context).stream()
