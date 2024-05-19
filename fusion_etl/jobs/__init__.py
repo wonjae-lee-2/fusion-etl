340256117d3ce05fa9703cf0781e0394575035ff
Wonjae Lee <wonjae.lee.2@gmail.com>
2024-05-19 17:10:48 +0000

    chore: rename tag keys

--- fusion_etl/jobs/__init__.py
+++ fusion_etl/jobs/__init__.py
@@ -1,12 +1,17 @@
-from dagster import define_asset_job, AssetSelection
+from dagster import AssetSelection, define_asset_job
 
 rdp_job = define_asset_job(
     "rdp_job",
     selection=AssetSelection.key_prefixes("rdp"),
-    tags={"target": "rdp"},
+    tags={"target_prefix": "rdp"},
 )
 dbt_job = define_asset_job(
     "dbt_job",
     selection=AssetSelection.key_prefixes("dbt"),
-    tags={"target": "dbt"},
+    tags={"target_prefix": "dbt"},
+)
+erp_job = define_asset_job(
+    "erp_job",
+    selection=AssetSelection.key_prefixes("erp"),
+    tags={"target_prefix": "erp"},
 )
