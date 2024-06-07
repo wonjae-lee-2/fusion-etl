a7ec35cde02555459c73ca9f6b14741815c73828
Wonjae Lee <wonjae.lee.2@gmail.com>
2024-06-07 13:42:04 +0000

    fix: run dbt run status sensor every 30 seconds

--- fusion_etl/sensors/dbt.py
+++ fusion_etl/sensors/dbt.py
@@ -10,7 +10,6 @@ from ..jobs import dbt_job, der_job, erp_active_job, erp_all_job, rdp_job
 
 @run_status_sensor(
     run_status=DagsterRunStatus.SUCCESS,
-    minimum_interval_seconds=60 * 60,
     monitored_jobs=[der_job, erp_active_job, erp_all_job, rdp_job],
     request_job=dbt_job,
 )
