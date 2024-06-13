9c64cdfd6991187c5f60aedd8dc21f9283bb73ef
Wonjae Lee <wonjae.lee.2@gmail.com>
2024-06-13 04:44:33 +0000

    fix: revise orion job schedule to run every 1:00 am UTC

--- fusion_etl/schedules/__init__.py
+++ fusion_etl/schedules/__init__.py
@@ -5,5 +5,5 @@ from ..jobs import orion_job
 orion_daily_schedule = ScheduleDefinition(
     name="orion_daily_schedule",
     job=orion_job,
-    cron_schedule="0 3 * * *",
+    cron_schedule="0 1 * * *",
 )
