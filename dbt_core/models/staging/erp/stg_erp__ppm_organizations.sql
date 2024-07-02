1aa942371f2adbea00362fcfdde9aabcb7bcd4a3
Wonjae Lee <wonjae.lee.2@gmail.com>
2024-07-02 10:51:38 +0000

    fix: update column names and order

--- dbt_core/models/staging/erp/stg_erp__ppm_organizations.sql
+++ dbt_core/models/staging/erp/stg_erp__ppm_organizations.sql
@@ -5,6 +5,7 @@ source as (
     select
         timestamp_utc,
         tree_version_name,
+        distance,
         dep24_pk1_value as expenditure_organization_id,
         dep30_pk1_name as field_hq,
         dep29_pk1_name as region,
